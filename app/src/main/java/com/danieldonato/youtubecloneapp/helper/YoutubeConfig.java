9349125b280bc096562c5fa197d59cf68df3eb95
DanielDonato <daniel_donato10@yahoo.com.br>
2019-11-15 17:22:24 -0300

    Configurando retrofit e youtube

---
+++ app/src/main/java/com/danieldonato/youtubecloneapp/helper/YoutubeConfig.java
@@ -0,0 +1,8 @@
+package com.danieldonato.youtubecloneapp.helper;
+
+public class YoutubeConfig {
+
+    public static String CALNAL_ID = "";
+    public static String CHAVE_YOUTUBE_API = "";
+    public static String URL_BASE = "";
+}

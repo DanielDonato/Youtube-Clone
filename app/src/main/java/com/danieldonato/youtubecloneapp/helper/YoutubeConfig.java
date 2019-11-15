ad7780a6f89f0d4aae3dd2d8b4a9147c5665dbe8
DanielDonato <daniel_donato10@yahoo.com.br>
2019-11-15 19:10:20 -0300

    Removendo api key do youtube

--- app/src/main/java/com/danieldonato/youtubecloneapp/helper/YoutubeConfig.java
+++ app/src/main/java/com/danieldonato/youtubecloneapp/helper/YoutubeConfig.java
@@ -3,6 +3,6 @@ package com.danieldonato.youtubecloneapp.helper;
 public class YoutubeConfig {
 
     public static String CALNAL_ID = "UCVHFbqXqoYvEWM1Ddxl0QDg";
-    public static String CHAVE_YOUTUBE_API = "AIzaSyBSPOq89iSOddl5ohFoUQ1z7jMRh46jo-g";
+    public static String CHAVE_YOUTUBE_API = "";
     public static String URL_BASE = "https://www.googleapis.com/youtube/v3/";
 }
